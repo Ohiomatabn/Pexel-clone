ebf16d713d9e53930233d6726dbbd28a93f8ccd2
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2024-12-26 22:31:52 +0100

    Make the download section interactive so that when the downlaod button is clicked the display the download section fixed on the page

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,7 +8,7 @@ images.forEach((image) =>{
         <div class = "overlay"></div>
         <img src="${image.src}" alt="IMG" loading="lazy">
         <div class="download">
-          <a href= "${image.src}" download="download"><img src="IMG/download.svg" class="download-icon">Download</a>
+          <button class="js-download-button"><img src="IMG/download.svg" class="download-icon">Download</button>
         </div>
         <div class="pexel-profile">
           <img src="IMG/ohiomata2.jpg" class="profile-img">
@@ -29,4 +29,12 @@ document.querySelector('.js-bars').addEventListener('click', ()=>{
 
 document.querySelector('.js-times').addEventListener('click', () =>{
   document.querySelector('.js-explore').setAttribute('style', 'margin: -200px');
+});
+
+document.querySelector('.js-download-button').addEventListener('click', () =>{
+  document.querySelector('.js-download-cover').setAttribute('style', 'display: block;');
+});
+
+document.querySelector('.js-hide').addEventListener('click', () =>{
+  document.querySelector('.js-download-cover').setAttribute('style', 'display: none;');
 });
