a914bcae976faa15ea642d7db9e99a96c60a12f8
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2024-12-26 22:32:27 +0100

    Add an id to each image object

--- JavaScript/images.js
+++ JavaScript/images.js
@@ -1,38 +1,50 @@
 export const images = [
   {src: "IMG/pexels-pixabay-237272.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0001'
   },
   {src: "IMG/pexels-tatianasyrikova-3933068.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0002'
   },
   {src: "IMG/pexels-pixabay-270366.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0003'
   },
   {src: "IMG/pexels-goumbik-574071.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0004'
   },
   {src: "IMG/pexels-aditya-bose-1283557-2450224.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0005'
   },
   {src: "IMG/pexels-asadphoto-457882.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0006'
   },
   {src: "IMG/pexels-freestockpro-2781814.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0007'
   },
   {src: "IMG/pexels-vince-2265876.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0008'
   },
   {src: "IMG/pexels-goumbik-574070.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0009'
   },
   {src: "IMG/pexels-pixabay-237272.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0010'
   },
   {src: "IMG/pexels-divinetechygirl-1181467.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0011'
   },
   {src: "IMG/pexels-pragyanbezbo-1720188.jpg",
-    likes: '22k'
+    likes: '22k',
+    id: 'img0012'
   },
 ];
